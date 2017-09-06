e26e227f0cb09ad439f6527a79805879d87d84fc
Gavin Stark <gavin@gstark.com>
2017-09-05 21:54:56 -0400

    Adds users + sign-up + sign-in

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  resources :questions
+
   get 'pages/welcome'
 
   root 'pages#welcome'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+  get '/login' => 'session#new'
+  post '/login' => 'session#create'
+  get '/logout' => 'session#destroy'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -21,4 +21,12 @@ ActiveRecord::Schema.define(version: 20170906012119) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
