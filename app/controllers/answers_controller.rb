fec2911c8c52d55ce82eca9a198ce01cdd659834
Gavin Stark <gavin@gstark.com>
2017-09-06 10:03:47 -0400

    Associates questions and answers to users

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,18 +1,24 @@
 class AnswersController < ApplicationController
-  
+
   def create
+    # Find the question first
     @question = Question.find(params[:question_id])
+
+    # Build a new answer for that question
     @answer = @question.answers.new(answer_params)
-    
+
+    # Associate the current user to this answer
+    @answer.user = current_user
+
     if @answer.save
       redirect_to @question, notice: "Answer created"
     else
       render 'questions/show'
     end
-  end  
-  
+  end
+
   private
-  
+
   def answer_params
     params.require(:answer).permit(:body)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+
+  has_many :questions
+  has_many :answers
 end
